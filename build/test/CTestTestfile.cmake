9ca792cdc9de5f030fcd06db808bd855361a7898
tzy0228 <117754269+tzy0228@users.noreply.github.com>
2023-10-20 12:24:48 +0800

    Delete build directory

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tzy/slambook2-master/myslam-SLAM/test
-# Build directory: /home/tzy/slambook2-master/myslam-SLAM/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_triangulation "test_triangulation")
